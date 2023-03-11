d7830aa4639c928eb3a1555ce3a9a349e5a810e8
sahmed <engsameergoumaa@gmail.com>
2023-03-11 19:45:35 +0300

    Fix

--- sudoko.go
+++ sudoko.go
@@ -1,7 +1,6 @@
 package main
+
 /* A program to solve a sudoku game using inputs from the terminal */
-
-
 
 import (
 	"fmt"
@@ -21,13 +20,13 @@ func main() {
 
 		// upon recieving a slution fill-out the created table
 		if FinalSolve(&table) == true {
-			for y := 0; y < 9; y++ {
-				for x := 0; x < 9; x++ {
-					if x != 8 {
-						z01.PrintRune(rune(table[y][x]))
-						z01.PrintRune(32)
+			for c := 0; c < 9; c++ {
+				for r := 0; r < 9; r++ {
+					if r != 8 {
+						fmt.Print(string(table[c][r]))
+						fmt.Print(" ")
 					} else {
-						z01.PrintRune(rune(table[y][x]))
+						fmt.Print(string(table[c][r]))
 					}
 				}
 				z01.PrintRune(10)
@@ -92,21 +91,21 @@ func IsItEmpty(table *[9][9]rune) bool {
 
 // check if the answer is valid
 
-func IsItValid(table *[9][9]rune, x int, y int, z rune) bool {
+func IsItValid(table *[9][9]rune, r int, c int, z rune) bool {
 	// check double int
 	for i := 0; i < 9; i++ {
-		if z == table[i][x] {
+		if z == table[i][r] {
 			return false
 		}
 	}
 	for j := 0; j < 9; j++ {
-		if z == table[y][j] {
+		if z == table[c][j] {
 			return false
 		}
 	}
 	// square check
-	a := x / 3
-	b := y / 3
+	a := r / 3
+	b := c / 3
 	for k := 3 * a; k < 3*(a+1); k++ {
 		for l := 3 * b; l < 3*(b+1); l++ {
 			if z == table[l][k] {
@@ -124,20 +123,20 @@ func FinalSolve(table *[9][9]rune) bool {
 		return true
 	}
 	// start the solve process
-	for y := 0; y < 9; y++ {
-		for x := 0; x < 9; x++ {
+	for c := 0; c < 9; c++ {
+		for r := 0; r < 9; r++ {
 			// check if the element is empty spot
-			if table[y][x] == '.' {
+			if table[c][r] == '.' {
 				for z := '1'; z <= '9'; z++ {
 					// check if the generated element is valid
-					if IsItValid(table, x, y, z) {
-						table[y][x] = z
+					if IsItValid(table, r, c, z) {
+						table[c][r] = z
 						// return the final table upon completion of the backtracking algorithm
 						if FinalSolve(table) {
 							return true
 						}
 					}
-					table[y][x] = '.'
+					table[c][r] = '.'
 				}
 				return false
 			}
